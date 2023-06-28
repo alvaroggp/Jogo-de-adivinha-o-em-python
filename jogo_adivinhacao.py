dee2bd41846270a31af7d266bbfcd03dfbe6f8ad
alvaroggp <131178055+alvaroggp@users.noreply.github.com>
2023-06-27 21:12:48 -0300

    adicionando arquivo jogo_adivinhacao

---
+++ jogo_adivinhacao.py
@@ -0,0 +1,50 @@
+from random import randint
+
+def titulos(titulo):
+    
+    tamanho_do_titulo = len(titulo) 
+
+    print("-" * tamanho_do_titulo)
+    print(titulo)
+    print("-" * tamanho_do_titulo)
+
+
+soma_dos_pontos = 0
+vezes_jogadas = 0
+
+while True:
+    pontuacao = 10    
+    titulos("Bem vindo ao jogo de adivinhação")
+
+    numero_certo = randint(1, 5)
+    numero_do_usuario = int(input('Digite um valor de 1 a 5: '))
+    
+    while True:
+        if numero_certo == numero_do_usuario:
+            print(f"Parabens, você acertou, o numero era {numero_certo} :)")
+            soma_dos_pontos += pontuacao
+            vezes_jogadas += 1
+            break
+        else: 
+            numero_do_usuario = int(input("Que pena você ERROU, tente de novo: "))
+            pontuacao -= 2
+            continue
+     
+    resposta = str(input("Quer continuar SIM/NAO:")).upper()
+    
+    while True:
+         if resposta == "SIM" or resposta == "NAO":
+            break
+         elif resposta != "SIM" or resposta != "NAO":
+            resposta = str(input("Perdao, Responda corretamente SIM/NAO: ")).upper()
+            continue
+       
+        
+    if resposta == "SIM":
+        continue
+    elif resposta == "NAO":
+        break 
+    
+media = soma_dos_pontos / vezes_jogadas 
+titulos(f"Muito obrigado por jogar, sua media de pontos foi {media:.2f}")
+        
